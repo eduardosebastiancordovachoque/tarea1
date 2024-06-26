a2c6d04a6e07c3909abd5c9b951eff63c0f008c1
eduardosebastiancordovachoque <145245472+eduardosebastiancordovachoque@users.noreply.github.com>
2024-06-25 19:20:14 -0500

    Add files via upload

---
+++ tarea 1/App.xaml.cs
@@ -0,0 +1,19 @@
+using Xamarin.Forms;
+
+namespace CurriculumVitaeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart() { }
+
+        protected override void OnSleep() { }
+
+        protected override void OnResume() { }
+    }
+}
---
+++ tarea 1/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+
+namespace CurriculumVitaeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
